10d14e5152947b8c7d6b0ec1986cbf8a8f0d99bb
acremers22 <117043863+acremers22@users.noreply.github.com>
2024-01-27 15:54:40 -0500

    Add files via upload

---
+++ ChartTest3BarChart.java
@@ -0,0 +1,155 @@
+/* Adapted From 
+ * 
+ * https://github.com/jfree/jfree-demos/blob/master/src/main/java/org/jfree/chart/demo/PieChartDemo1.java
+ *
+ * and
+ *
+ * http://www.jfree.org/phpBB2/viewtopic.php?f=3&t=25969
+ */
+/* ----------------------------
+* LegendTitleToImageDemo2.java
+* ----------------------------
+* (C) Copyright 2008, by Object Refinery Limited.
+*
+*/
+
+package test;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.RadialGradientPaint;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.jfree.chart.block.RectangleConstraint;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.chart.ui.HorizontalAlignment;
+import org.jfree.chart.ui.RectangleEdge;
+import org.jfree.chart.ui.RectangleInsets;
+import org.jfree.chart.ui.WindowUtils;
+import org.jfree.chart.ui.UIUtils;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.Range;
+
+/**
+* Here we save a legend to a PNG file...the legend has a lot of items, so we
+* apply a width constraint so it doesn't get too wide.
+*/
+public class ChartTest1 {
+
+  private static JFreeChart createChart(BarDataset dataset) {
+
+	  JFreeChart chart = ChartFactory.createBarChart(
+	      "Bar Chart Test",  // chart title
+          "Category",
+          "Value",
+          dataset
+	  );
+      CategoryPlot plot = (CategoryPlot) chart.getPlot();
+      NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+      rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+	  return chart;
+  }
+
+    private static BarDataset createDataset() {
+        // row keys...
+        String series1 = "First";
+        String series2 = "Second";
+        String series3 = "Third";
+
+        // column keys...
+        String category1 = "Category 1";
+        String category2 = "Category 2";
+        String category3 = "Category 3";
+        String category4 = "Category 4";
+        String category5 = "Category 5";
+
+        // create the dataset...
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(1.0, series1, category1);
+        dataset.addValue(4.0, series1, category2);
+        dataset.addValue(3.0, series1, category3);
+        dataset.addValue(5.0, series1, category4);
+        dataset.addValue(5.0, series1, category5);
+
+        dataset.addValue(5.0, series2, category1);
+        dataset.addValue(7.0, series2, category2);
+        dataset.addValue(6.0, series2, category3);
+        dataset.addValue(8.0, series2, category4);
+        dataset.addValue(4.0, series2, category5);
+
+        dataset.addValue(4.0, series3, category1);
+        dataset.addValue(3.0, series3, category2);
+        dataset.addValue(2.0, series3, category3);
+        dataset.addValue(3.0, series3, category4);
+        dataset.addValue(6.0, series3, category5);
+
+        return dataset;
+    }
+
+    /**
+     * Entry point.
+     *
+     * @param args  command line arguments (ignored).
+     *
+     * @throws IOException if there is an input/output problem.
+     */
+    public static void main(String[] args) throws IOException {
+        BarDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset); 
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setFillZoomRectangle(true);
+        chartPanel.setMouseWheelEnabled(true);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+        
+        setContentPane(chartPanel);
+
+        // we need to layout the legend to know how much space it requires
+        // note that it is also possible to call arrange() with some
+        // constraints on the layout
+        BufferedImage img = new BufferedImage(1, 1,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = img.createGraphics();
+        g2.dispose();
+
+        // now create an image of the required size for the legend
+        /*int w = (int) Math.rint(800);
+        int h = (int) Math.rint(600);
+        BufferedImage img2 = new BufferedImage(w, h,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g22 = img2.createGraphics();
+        chart.draw(g22, new Rectangle2D.Double(0, 0, w, h));
+        g22.dispose();
+        */
+
+        // ...and save it to a PNG image
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(
+                new File("output1.png")));
+        ChartUtils.writeBufferedImageAsPNG(out, img);
+        out.close();
+        System.out.println("output1.png created"); 
+
+        
+    }
+}
